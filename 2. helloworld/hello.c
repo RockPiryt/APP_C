49e50be270277b54d46e0fd78966cc62a5aa46a2
Paulina Kimak <paulina.kimak.oferty@gmail.com>
2023-11-27 12:01:37 +0100

    'RockPiryt' - add app: add 2 2 numbers

--- 2. helloworld/hello.c
+++ 2. helloworld/hello.c
@@ -3,5 +3,5 @@
 int main()
 {
 	printf("Hello World");
-	return 1234;
+	return 0;
 }
---
+++ 3.add_2_values/add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter number 1:\n");
+    scanf("%d", &num1);
+
+    printf("Enter number 2:\n");
+    scanf("%d", &num2);
+
+    printf("%d=%d=%d", num1, num2, num1+num2);
+
+    return 0;
+
+}
