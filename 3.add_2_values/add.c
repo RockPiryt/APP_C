a2d8ebf7f927bc0421e61efa020bf75b0baa8349
Paulina Kimak <paulina.kimak.oferty@gmail.com>
2023-11-27 12:08:40 +0100

    "RockPiryt' - add app: add 2 2 float number

--- 3.add_2_values/add.c
+++ 3.add_2_values/add.c
@@ -10,7 +10,7 @@ int main()
     printf("Enter number 2:\n");
     scanf("%d", &num2);
 
-    printf("%d=%d=%d", num1, num2, num1+num2);
+    printf("%d+%d=%d", num1, num2, num1+num2);
 
     return 0;
 
