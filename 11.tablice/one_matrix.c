a1a31dead2847b6596b3e6cabbedf624693f20ad
Paulina Kimak <paulina.kimak.oferty@gmail.com>
2024-01-07 07:17:33 +0100

    'RockPiryt' - add program odd_even and make changes in one_matrix

--- 11.tablice/one_matrix.c
+++ 11.tablice/one_matrix.c
@@ -8,7 +8,7 @@ void print_array(int *array, int tab_size);
 
 int find_max(int *array, int tab_size);
 double calc_average(int *array, int tab_size);
-void odd_even();
+void odd_even(int *array, int tab_size);
 
 int main()
 
@@ -37,6 +37,9 @@ int main()
     //Calc average of array values
     average = calc_average(user_array,n); 
     printf("Average is: %.2lf\n", average);
+
+    // Check odd and even
+    odd_even(user_array,n);
 
     free(user_array);
 
@@ -100,15 +103,33 @@ double calc_average(int *array, int tab_size)
 
     return average;
 }
-void odd_even()
-{   int odd=1;
-    int even=0;//parzysty
+void odd_even(int *array, int tab_size)
+{   
 
-    if (odd == 1 && even == 1)
+    //Add odd numbers to array
+    int odd_count = 0;//nieparzysty
+    for(int i=0; i<tab_size; i++)
+    if (array[i] % 2 ==1)
+    {
+        odd_count++;
+    }
+
+    //Add even numbers to array
+    int even_count = 0; //parzysty
+    for(int i=0; i<tab_size; i++)
+    if (array[i] % 2 == 0)
+    {
+        even_count++;
+    }
+
+    printf("Number of odd digits: %d\n", odd_count);
+    printf("Number of even digits: %d\n", even_count);
+
+    if (odd_count != 0 && even_count != 0)
     {
         printf("Matrix has odd end even digits");
     }
-    else if(odd == 1 && even == 0)
+    else if(odd_count != 0 && even_count == 0)
     {
         printf("Matrix has only odd digits");
     }
