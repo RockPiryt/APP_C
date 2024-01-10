4b2f5ecad639d03c1c1689a00bfd457e8eaf386f
PaulinaKimak <p.kimak.047@studms.ug.edu.pl>
2024-01-10 17:03:50 +0100

    'RockPiryt' - add program find_num_binary and make changes in sum_binary

--- 16.Algorithms/exercise/find_num_binary_search.c
+++ 16.Algorithms/exercise/find_num_binary_search.c
@@ -9,14 +9,12 @@ int *allocate_array(int n); //Allocate space for array, return array
 int *user_fill_array(int n); // Fill array with values, return array
 int print_array(int *array, int n ); // Print array
 int *bubble_asce(int *array, int n); // Sort unsorted array ascending, return array
-int calc_sum(int *array, int n);// Sum values in array
-int binary_search_sum(int array[], int l, int r); // Sum values in array with binary search
-int binary_search(int array[], int x, int l, int r);//Find num in array
+int binary_search(int array[], int x, int l, int r);// to find index o number, which we are looking for
+
 int main()
 {   int *unsorted_array;
     int *sorted_array;
     int summary;
-    int binary_summary;
     int n=0;
     int num;
 
@@ -49,22 +47,14 @@ int main()
 
     printf("\n");
 
-
+    
     //User enter number to find in array
     printf("Please number to find in array:\n");
     scanf("%d", &num);
 
     int end = n-1;
-    int index = binary_search(sorted_array, num, 0, end);//0 start index, "end" end index
-    printf("Index of your number %d in your array is: %d\n", num, index);
-
-    //Calc standard summary of array values
-    summary = calc_sum(sorted_array, n); 
-    printf("Summary of array values is: %d\n", summary);
-
-    //Calc binary summary of array values
-    binary_summary = binary_search_sum(sorted_array, 0, end); 
-    printf("Binary sum: %d\n", binary_summary);
+    int result = binary_search(sorted_array, num, 0, end);//0 start index, "end" end index
+    printf("Your number %d is in array(1-YES, 0-NO): %d\n", num, result);
 
     //Release space memory
     free(sorted_array);
@@ -86,7 +76,7 @@ int *allocate_array(int n)
     //Check if array has allocated memory
     if (array == NULL)
     {
-      printf("error");
+      printf("BŁĄD");
       return 0;
     }
     else
@@ -151,36 +141,22 @@ int calc_sum(int *array, int n)
 // FUNCTION to find index o number, which we are looking for
 int binary_search(int array[], int x, int l, int r)
 {
-  // Index of middle point
-  int mid = l + (r - l) / 2;
-  
-  // When we search all array and didn't find x
-  if (l > r) 
-  return -1;
-  
-  //Check the indexes in array
-  if (array[mid] == x) //middle point=x
-    return mid;
-  else if (array[mid] > x)// x in left side of middle point
-    return binary_search(array, x, l, mid - 1);
-  else// x in right side of middle point
-    return binary_search(array, x, mid + 1, r);
+    // Index of middle point
+    int mid = l + (r - l) / 2;
+    
+    // When we search all array and didn't find x
+    //if (l > r) return -1;
+    if (l > r) return 0;
+    
+    //Check the indexes in array
+    if (array[mid] == x) //middle point=x
+        //return mid;
+        return 1;
+    else if (array[mid] > x)// x in left side of middle point
+        return binary_search(array, x, l, mid - 1);
+    else// x in right side of middle point
+        return binary_search(array, x, mid + 1, r);
+
+
 }
 
-// FUNCTION to calculate sum using binary_search
-int binary_search_sum(int array[], int l, int r) 
-{
-    // One element in array
-    if (l == r) 
-        return array[l];
-
-    // Middle point of array
-    int middle = (l + r) / 2;
-
-    // Calc left and right value
-    int leftSum = binary_search_sum(array, l, middle);
-    int rightSum = binary_search_sum(array, middle + 1, r);
-
-    // Add left and right values
-    return leftSum + rightSum;
-}
