08d460e1e19b53490f25dc156dfd8919f9f5ea18
Paulina Kimak <rockpiryt@gmail.com>
2024-01-24 21:54:03 +0100

    'RockPiryt' - make changes in struct_info program

--- 21.struct/struct_info.c
+++ 21.struct/struct_info.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+void print_student(Student student);
+
 // struct Student
 //     {   
 //         char name[50];
@@ -10,39 +12,63 @@
     
 //     };
 
-  typedef struct
-    {   
-        char name[50];
-        char id[10];
-        int age;
-        int grades[5];
-    
-    } Student;  
+typedef struct
+{   
+    char name[50];
+    char id[10];
+    int wiek;
+    int grades[5];
+
+} Student;  
+
+typedef struct
+{
+    int x;
+    int y;
+} Point;
 
 
 int main()
 {   
-    // struct Student pawel;
-    Student pawel;
+    //Create struct instance
+    // struct Student student_a;
+    Student osoba;
 
-    pawel.age = 38;
-    strcpy(pawel.name, "Pawel");
-    strcpy(pawel.id, "yy123kkll34");
-    pawel.grades[0] = 1;
-    pawel.grades[1] = 2;
-    pawel.grades[2] = 3;
-    pawel.grades[3] = 4;
-    pawel.grades[4] = 5;
+    //Add values to attributes
+    osoba.wiek = 22;
+    strcpy(osoba.name, "Paulina");
+    strcpy(osoba.id, "yy123kkll34");
+    osoba.grades[0] = 1;
+    osoba.grades[1] = 2;
+    osoba.grades[2] = 3;
+    osoba.grades[3] = 4;
+    osoba.grades[4] = 5;
 
-    printf("Student age: %d\n", pawel.age);
-    printf("Student name: %s\n", pawel.name);
-    printf("Student ID: %s\n", pawel.id);
-    printf("Student Grade[0]: %d\n", pawel.grades[0]);
+    // //Call function to print info about student
+    print_student(osoba);
+
+    //Simple printing of student attributes
+    //printf("%d", osoba.wiek);
+    // printf("Student name: %s\n", osoba.name);
+    // printf("Student ID: %s\n", osoba.id);
+    // printf("Student Grade[0]: %d\n", osoba.grades[0]);
+    // printf("Student Grades: ");
+    // for(int i=0; i<5; i++)
+    //     {
+    //         printf("%d ", osoba.grades[i]);
+    //     }
+
+    return 0;
+}
+void print_student(Student student)
+{
+    printf("Student age is: %d\n", student.wiek);
+    printf("Student name: %s\n", student.name);
+    printf("Student ID: %s\n", student.id);
+    printf("Student Grade[0]: %d\n", student.grades[0]);
     printf("Student Grades: ");
     for(int i=0; i<5; i++)
         {
-            printf("%d ", pawel.grades[i]);
+            printf("%d ", student.grades[i]);
         }
-
-    return 0;
 }
