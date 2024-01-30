8cac0c8c9e5d10c12446c24e2239a073cee7667a
Paulina Kimak <rockpiryt@gmail.com>
2024-01-30 05:56:09 +0100

    'RockPiryt' - add book program and 2 test program

---
+++ 22.egzamin/1_test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//odpowiedź 2 tylko wyprintuje text
+
+int main()
+{   
+    
+    //if ('n'== 0) printf("Tak 0");
+    if ('n') printf("Tak n");
+    //if (!'n'== 'n') printf("Tak zaprzeczenie");
+    //if ('n'== "n") printf("Tak n cudzyslow");
+
+    return 0;
+
+}
